8c8ce99500d8165c91224a193f9444cb5c8bd366
Jenny Kitchen <jennykayekitchen@gmail.com>
2023-04-11 15:28:59 -0500

    more project setup

--- Streamish/Startup.cs
+++ Streamish/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using Streamish.Repositories;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -26,7 +27,7 @@ namespace Streamish
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddTransient<IVideoRepository , VideoRepository>();
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
